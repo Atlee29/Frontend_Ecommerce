4c105db41485bf2d9fa7b3a2bab4ca2d0cf4be63
siddharth30101997 <siddharthjain285@gmail.com>
2023-06-23 10:22:08 +0530

    dash component change

--- Ecommerce_Project/src/app/modules/customer/customer-routing.module.ts
+++ Ecommerce_Project/src/app/modules/customer/customer-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewproductsForCustomerComponent } from './viewproducts-for-customer/viewproducts-for-customer.component';
+import { ViewSingleProductComponent } from './view-single-product/view-single-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'viewProductsCustomer',component:ViewproductsForCustomerComponent},
+  {path:'viewSingleProduct/:productId',component:ViewSingleProductComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
